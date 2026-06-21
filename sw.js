2b50a95e27d726a7abe251b31660554acf6b7476
ARASH <as.m3.arashfadaei@gmail.com>
2026-06-21 17:32:18 +0330

    Add PWA manifest, service worker, and icons for PWABuilder

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'espadana-v1';
+const ASSETS = [
+  '/espadana-invoice/',
+  '/espadana-invoice/index.html',
+  '/espadana-invoice/assets/stamp.jpg',
+  '/espadana-invoice/assets/SIGNATURE.jpg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
